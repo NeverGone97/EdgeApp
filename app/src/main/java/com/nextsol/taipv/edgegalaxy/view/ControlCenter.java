0e83ca9f5e06bd2131d5744c2522138d240b1db2
tai phung <tai97nd@gmail.com>
2018-04-26 17:32:39 +0700

    control center

--- app/src/main/java/com/nextsol/taipv/edgegalaxy/view/ControlCenter.java
+++ app/src/main/java/com/nextsol/taipv/edgegalaxy/view/ControlCenter.java
@@ -1,14 +1,19 @@
 package com.nextsol.taipv.edgegalaxy.view;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +21,17 @@ import android.widget.SeekBar;
 
 import com.nextsol.taipv.edgegalaxy.R;
 
+import static android.content.Context.AUDIO_SERVICE;
+import static com.google.android.gms.plus.PlusOneDummyView.TAG;
+
 public class ControlCenter extends Fragment {
     private SeekBar sbBrightness, sbVollumn;
+    int x,y;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkPermissionForReadExtertalStorage();
-        try {
-            requestPermissionForReadExtertalStorage();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Nullable
@@ -39,15 +43,41 @@ public class ControlCenter extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+//        checkPermissionForReadExtertalStorage();
+//        try {
+//            requestPermissionForReadExtertalStorage();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         initView(view);
-        initEvent();
+        try {
+            initEvent();
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initEvent() {
+    private void initEvent() throws Settings.SettingNotFoundException {
+
+        int oldBrightness = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS);
+        Log.d("xxx", "initEvent: "+oldBrightness);
+        sbBrightness.setProgress((int)(oldBrightness/2.55));
         sbBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Settings.System.putInt(getContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 1  0);
+                Context context = getContext();
+
+                // Check whether has the write settings permission or not.
+                boolean settingsCanWrite = hasWriteSettingsPermission(context);
+                // If do not have then open the Can modify system settings panel.
+                 x = (int) (progress * 2.55);
+                if (!settingsCanWrite) {
+                    changeWriteSettingsPermission(context);
+                } else {
+                    changeScreenBrightness(context, x);
+                }
             }
 
             @Override
@@ -57,7 +87,26 @@ public class ControlCenter extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                seekBar.setProgress((int)(x/2.55));
+            }
+        });
+        final AudioManager am = (AudioManager) getContext().getSystemService(AUDIO_SERVICE);
+        final int volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        Log.d("xxx", "initEvent:vollumn "+volume_level);
+        sbVollumn.setProgress((int)(volume_level*100/15));
+        sbVollumn.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                am.setStreamVolume(AudioManager.STREAM_MUSIC,(int)(progress*15/100),0);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
     }
@@ -84,4 +133,36 @@ public class ControlCenter extends Fragment {
             throw e;
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean hasWriteSettingsPermission(Context context) {
+        boolean ret = true;
+        // Get the result from below code.
+        ret = Settings.System.canWrite(context);
+        return ret;
+    }
+
+    // Start can modify system settings panel to let user change the write settings permission.
+    private void changeWriteSettingsPermission(Context context) {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        context.startActivity(intent);
+    }
+
+    // This function only take effect in real physical android device,
+    // it can not take effect in android emulator.
+    private void changeScreenBrightness(Context context, int screenBrightnessValue) {
+        // Change the screen brightness change mode to manual.
+        Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        // Apply the screen brightness value to the system, this will change the value in Settings ---> Display ---> Brightness level.
+        // It will also change the screen brightness for the device.
+        Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, screenBrightnessValue);
+
+        /*
+        Window window = getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.screenBrightness = screenBrightnessValue / 255f;
+        window.setAttributes(layoutParams);
+        */
+    }
+
 }
